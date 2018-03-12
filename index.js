66ce30b700ec6c5c50f25a2ce67580f21d74b2df
messifrd7 <37272550+messifrd7@users.noreply.github.com>
2018-03-12 12:19:20 +0330

    Update index.js

--- index.js
+++ index.js
@@ -7,34 +7,14 @@ const CoinHive = require('coin-hive');
       pass: 'x' // default 'x' if not provided
     }
   });
- 
-  // Start miner
   await miner.start();
- 
-  // Listen on events
-  miner.on('found', () => console.log('Found!!'))
-  miner.on('accepted', () => console.log('Accepted!!'))
-  miner.on('update', data => console.log(`
+  miner.on('found', () => console.log('Found!'));
+  miner.on('accepted', () => console.log('Accepted!'));
+  miner.on('update', data =>
+    console.log(`
     Hashes per second: ${data.hashesPerSecond}
     Total hashes: ${data.totalHashes}
     Accepted hashes: ${data.acceptedHashes}
-  `));
- 
-  const requestHandler = (request, response) => {  
-    console.log(request.url)
-    response.end('Running the Monero Miner!!')
-  }
-
-  const server = http.createServer(requestHandler)
-
-  server.listen(process.env.PORT, (err) => {  
-    if (err) {
-      return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening`)
-  })
-
-  // Stop miner
-  //setTimeout(async () => await miner.stop(), 60000);
+  `)
+  );
 })();
